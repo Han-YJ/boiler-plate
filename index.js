b920a00896918d0b7b792fde8f4ed654b9f40d29
Han <lucirhk@gmail.com>
2021-03-09 17:37:34 +0900

    Add Nodemon

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/Users");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://han:slfjdkl11@boilerplate.4mrni.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -12,6 +20,21 @@ mongoose.connect('mongodb+srv://han:slfjdkl11@boilerplate.4mrni.mongodb.net/myFi
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+app.post('/register', (req, res) => {
+  //회원 가입 시 필요 정보들을 client에서 가져오면 db에 넣어준다.
+
+  const user = new User(req.body)
+  user.save((err, userInfo) => {
+    if(err) return rs.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
